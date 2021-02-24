c132b2bda3a4fffafdd5576942ec2267de5853e7
André Barbosa <andrebarbosa.dev@gmail.com>
2021-02-24 16:51:34 -0300

    Project refactoring to next.js using components created at first class.

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from "../components/ExperienceBar"
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar></ExperienceBar>
+      <h1>Hello NLW!</h1>
+    </div>
+  )
+}

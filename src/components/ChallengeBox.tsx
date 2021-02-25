a6bc2f14ce575695d6f6be00f690fc5a1306e909
André Barbosa <andrebarbosa.dev@gmail.com>
2021-02-25 16:09:03 -0300

    Challenge Context and functions.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            { activeChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
+                    <main>
+                        <img src={`icons/${activeChallenge.type}.svg"`} alt="" />
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>
+
+                        <footer>
+                            <button type="button" onClick={resetChallenge} className={styles.challengeFailedButton}>Falhei</button>
+                            <button type="button" className={styles.challengeSuccededButton}>Completei</button>
+                        </footer>
+                    </main>
+                </div>
+            ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="level up" />
+                        Avance de nível completando desafios.
+                    </p>
+                    </div>
+                )}
+        </div>
+    );
+}
